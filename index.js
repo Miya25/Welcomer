01ebd8fccad72f4ebbb082107f8ecc6e7e2c2d8c
Miya <foenx0315@gmail.com>
2022-04-25 06:41:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,9 +32,3 @@ welcomeCard.build(),"welcome.png")
 })
 
 client.login(process.env.TOKEN)
-
-//https://cdn.discordapp.com/attachments/893113075534798858/906893762737889320/Untitled123_20211107122709.png
-//https://cdn.discordapp.com/attachments/843060364358451200/875438575490060298/king_baybars4.jpg
-////https://cdn.discordapp.com/attachments/843060364358451200/875438574659575899/1606680255792.png
-//https://cdn.discordapp.com/attachments/843060364358451200/875284117716086834/Efektler_-_Kadir_Gzltp_50.jpg
-//
